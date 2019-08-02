1e7b7e0f9eb57138b793d630dd9eb77bf8e0f9eb
Fabian1992 <fabi.lopez1992@gmail.com>
2019-08-02 15:23:36 -0500

    fin estaciones

--- app/Http/Controllers/Estaciones.php
+++ app/Http/Controllers/Estaciones.php
@@ -16,7 +16,7 @@ class Estaciones extends Controller
 {
 	public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth','permission:G. de estaciones']);
     }
 
 	public function index(EstacionDataTable $dataTable)
