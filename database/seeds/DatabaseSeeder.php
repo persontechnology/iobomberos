abb0d4670b8e029387107712a407688f3df522bc
Fabian1992 <fabi.lopez1992@gmail.com>
2019-08-21 11:43:38 -0500

    clinicas ingresar

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(EmergenciaTableSeeder::class);
         $this->call(TipoEmergenciaTableSeeder::class);
+        $this->call(ClinicasSeeder::class);
     }
 }
